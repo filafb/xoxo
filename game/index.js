b78a8beb9bd2f4deb0a9734a9c561b57dc9323d4
Kenneth Lai <knthslai@Kenneths-MacBook-Pro-2.local>
2018-07-05 13:11:28 -0400

    finished move

--- game/index.js
+++ game/index.js
@@ -1,6 +1,29 @@
 import {Map} from 'immutable'
 
-export default function reducer(state, action) {
+export default function reducer(state = {turn:'X',board:Map()}, action) {
   // TODO
-  return state
+    console.log('action: ',action)
+    let nextPlayer = ''
+    if(action.turn==='X'){nextPlayer='O'}
+    else {nextPlayer = 'X'}
+
+  switch (action.type){
+    // console.log(''action.move)
+
+    case 'move':
+      return {
+        turn: nextPlayer,
+        board: state.board.setIn(action.coords,action.turn)}
+    default:
+      return state
+  }
 }
+
+export function move(turn,coords){
+  return {
+    type:'move',
+    turn: turn,
+    coords:coords
+  }
+}
+
