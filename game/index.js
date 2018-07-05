0e061b1ee1fea4183dca5e33ed3c21d723481421
Kenneth Lai <knthslai@Kenneths-MacBook-Pro-2.local>
2018-07-05 16:10:31 -0400

    finished winner

--- game/index.js
+++ game/index.js
@@ -2,15 +2,22 @@ import {Map} from 'immutable'
 
 export default function reducer(state = {turn:'X',board:Map()}, action) {
   // TODO
-    console.log('action: ',action)
-    let nextPlayer = ''
-    if(action.turn==='X'){nextPlayer='O'}
-    else {nextPlayer = 'X'}
+    // console.log('board: ',state.board)
+    // console.log('row: ',state.board['0'])
+    // console.log('row: ',state.board['0']['0'])
+    // if(state.board.getIn(['0','0'])){
+
+    // }
+
 
   switch (action.type){
     // console.log(''action.move)
 
     case 'move':
+      let nextPlayer = ''
+      if(action.turn==='X'){nextPlayer='O'}
+      else {nextPlayer = 'X'}
+    // console.log(state.board.getIn([0,0]))
       return {
         turn: nextPlayer,
         board: state.board.setIn(action.coords,action.turn)}
@@ -27,3 +34,14 @@ export function move(turn,coords){
   }
 }
 
+export function streak(board,coords) {
+  // TOOD
+  const result = board.getIn(coords[0])
+  for(let i = 0; i<coords.length;i++ ){
+    const currValue = board.getIn(coords[i])
+    if(currValue!==result){
+      return undefined
+    }
+  }
+  return result
+}
--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 import inquirer from 'inquirer'
 
-import gameReducer, {move} from './game'
+import gameReducer, {move, streak} from './game'
 import {createStore} from 'redux'
+let counter = -1
 
 const printBoard = () => {
+
+  if (winner()){
+  console.log(winner(),'- Won!')}
+  // console.log('counter: ',counter)
   const {board} = game.getState()
   for (let r = 0; r != 3; ++r) {
     for (let c = 0; c != 3; ++c) {
@@ -13,8 +18,53 @@ const printBoard = () => {
   }
 }
 
+const winner = () =>{
+  if (counter === 9){
+    return 'Computer'
+  }else{
+  counter+= 1}
+  // console.log('counter: ',counter)
+  const {board} = game.getState()
+  //streak(board,[0,0],coords) coords = [[0,1],[0,2]]
+  // let firstCoord = [0,0]
+  //|============== Row Check ==============|
+  for(let row = 0;row<3;row++){
+    let arrCoords = []
+    for(let col = 0;col<3;col++){
+      arrCoords.push([row,col])
+    }
+    const resultWinner = streak(board,arrCoords)
+    if(resultWinner!==undefined){
+      return resultWinner
+    }
+  }
+  //|============== Column Check ==============|
+  for(let col = 0;col<3;col++){
+    let arrCoords = []
+    for(let row = 0;row<3;row++){
+      arrCoords.push([row,col])
+    }
+    const resultWinner = streak(board,arrCoords)
+    if(resultWinner!==undefined){
+      return resultWinner
+    }
+  }
+  //|============== backward slash ==============|
+    const bSlashRes = streak(board,[[0,0],[1,1],[2,2]])
+    if(bSlashRes){
+      return bSlashRes
+    }
+  //|============== forward slash ==============|
+    const fSlashRes = streak(board,[[2,0],[1,1],[0,2]])
+    if(fSlashRes){
+      return fSlashRes
+    }
+
+
+}
+
 const getInput = player => async () => {
-  const {turn} = game.getState()  
+  const {turn} = game.getState()
   if (turn !== player) return
   const ans = await inquirer.prompt([{
     type: 'input',
